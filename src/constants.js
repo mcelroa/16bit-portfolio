7edbb32dc7a77c55721b1d17178c611bb4eecc39
Adam McElroy <adam.mcelroy15@gmail.com>
2024-05-16 22:19:29 +0100

    upload project

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: "This is my PC where I sit and spend my time coding. I enjoy coding in many languages such as Python, JavaScript & C#. Outside of my time coding I also like to Game on my PC. You can see some of the things I have coded <a href='https://github.com/mcelroa' target='_blank'>here</a>!",
+  "cs-degree": "This is my level 8 in Software Engineering I obtained from DCU in 2020!",
+  "sofa-table":
+    "This is my sofa where I like to sit back and unwind with some YouTube or Netflix when I have time",
+  projects:
+    "This is a list of my projects which can be seen on my GitHub. You can access it from my PC over there. I have created a Netflix clone in React, A backend API and frontend for a blog application in Node and Express. I have also made a game using KaboomJS which just so happens to be how I created this portfolio too!",
+  library:
+    "There are a lot of technical books on my shelf about topics which I have knowledge in. There are books here about SQL, MongoDB, Web Development, Git and many popular frameworks such as React and Angular",
+};
